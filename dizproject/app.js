87815c5a5ea21a29c8550290dbbe009f023667c1
KayBi <kaybhack@gmail.com>
2016-10-12 18:11:58 +0000

    première modifications de app.js pour installer Express et rooting

--- dizproject/app.js
+++ dizproject/app.js
@@ -1,2 +1,17 @@
-var aloha = 'Hello world !';
-console.log(aloha);
+//Récupération du package Express et instanciation
+var express = require('express');
+var app  = express();
+
+//Routing
+app.get('/',function(req,res){
+    res.send('Bonjour World !');
+});
+
+//Définition et vérification du port
+var port = process.env.PORT; 
+app.listen(port, function(err){
+    console.log('server is running on port: '+ port);
+});
+
+
+
